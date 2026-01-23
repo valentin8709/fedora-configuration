9331dfbe11cec5b64395042087ad30bc2ff5460e
Valou <valentin8709@users.noreply.github.com>
2026-01-23 18:02:21 +0100

    Add auto install script

---
+++ hyprland/install.sh
@@ -0,0 +1,30 @@
+# Hyprland UserConfig Setup
+# By Valou
+
+echo ""
+echo "=== Hyprland UserConfig Setup ==="
+echo ""
+
+echo "Setup 01-UserDefaults.conf..."
+cp UserConfigs/01-UserDefaults.conf ~/.config/hypr/UserConfigs/
+echo "Done"
+echo ""
+
+echo "Setup UserKeyinds.conf..."
+cp UserConfigs/UserKeybinds.conf ~/.config/hypr/UserConfigs/
+echo "Done"
+echo ""
+
+echo "Setup Rofi search config..."
+cp rofi/config-search.rasi ~/.config/rofi/
+echo "Done"
+echo ""
+
+echo "Setup wlogout"
+cp wlogout/* ~/.config/wlogout/
+echo "Done"
+echo ""
+
+echo "=== Hyprland UserConfig Setup Done ==="
+echo ""
+
